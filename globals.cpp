5acafa318b350eccb86ad21173d1eb659c0f1a87
ratsareweird <alecninja5000@gmail.com>
2024-12-12 15:46:40 -0500

    Update globals.cpp
    Other new stuff

--- globals.cpp
+++ globals.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <cstdint>
 #include <stdexcept>
+#include <string>
 #include <vector>
 
 
@@ -17,8 +18,8 @@ std::vector<pros::Motor> drive_left_group = {left_front,left_middle,left_back};
 std::vector<pros::Motor> drive_right_group = {right_front,right_middle,right_back};
 
 // intake
-pros::Motor left_intake(20, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor right_intake(12, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor left_intake(20, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor right_intake(12, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
 
 // conveyor
 pros::Motor conveyor(18, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
@@ -27,21 +28,20 @@ pros::Motor conveyor(18, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_D
 pros::ADIDigitalOut clamp('E');
 
 // lift
-pros::ADIDigitalOut right_lift('F');
+pros::ADIDigitalOut lift('F');
 
+// ejector
+pros::ADIDigitalOut ejector('G');
 
 // inertia sensor
 pros::Imu imu(17);
-
-// clamp sensor
-pros::ADIDigitalIn clamp_sensor('H');
 
 // optical shaft encoder
 pros::c::adi_encoder_t left_drive_encoder = pros::c::adi_encoder_init('C', 'D', false);
 pros::c::adi_encoder_t right_drive_encoder = pros::c::adi_encoder_init('A', 'B', true);
 
 // vision sensor
-pros::Vision vision_sensor(19, pros::E_VISION_ZERO_CENTER);
+pros::Vision vision_sensor(9, pros::E_VISION_ZERO_CENTER);
 
 
 // controller
@@ -52,6 +52,7 @@ const double PI = 3.1415926;
 
 const double DRIVE_TRAIN_GEAR_RATIO = 3.0 / 5.0;
 const double WHEEL_RADIUS = 1.625;
+
 
 
 // helper functions
@@ -65,7 +66,7 @@ void group_speed(std::vector<pros::Motor> group, int speed) {
 }
 
 /**
-Sets the speed of each side of the drivetrain
+Sets the speed of each side of the chassis
 */
 void wheels_speed(int left_speed, int right_speed) {
   group_speed(drive_left_group, left_speed);
@@ -94,6 +95,7 @@ int sign(double value) {
     return 0;
 }
 
+
 /**
 Returns inches in terms of degrees turned
 */
@@ -102,7 +104,7 @@ double degrees_to_inches(int degrees, double radius) {
 }
 
 /**
-Gets the inches traveled an encoder connected to the drivetrain
+Gets the inches that an encoder has registered that the chassis has moved
 */
 double degrees_to_drive_inches(pros::c::adi_encoder_t encoder) {
   return degrees_to_inches(pros::c::adi_encoder_get(encoder), WHEEL_RADIUS);
@@ -121,7 +123,7 @@ Waits until a motor's velocity is zero
 */
 void wait_until_motor_move_complete(pros::Motor motor) {
   while (motor.get_actual_velocity() > 0) {
-    pros::delay(10);
+    pros::Task::delay(10);
   }
 }
 
@@ -200,7 +202,11 @@ void drive_wheels() {
 
 pros::controller_digital_e_t intake_button = DIGITAL_R2;
 pros::controller_digital_e_t extake_button = DIGITAL_R1;
+
+
 void drive_intake() {
+
+
   if (controller.get_digital(intake_button)) {
     left_intake = 127;
     right_intake = 127;
@@ -216,40 +222,44 @@ void drive_intake() {
     right_intake = 0;
     conveyor = 0;
   }
+
+  
+
 }
 
 
-pros::controller_digital_e_t clamp_button = DIGITAL_A;
+pros::controller_digital_e_t clamp_button = DIGITAL_L1;
+pros::controller_digital_e_t alt_clamp_button = DIGITAL_L2;
+
 void drive_clamp() {
   static bool on = false;
 
-  if (controller.get_digital_new_press(clamp_button)) {
+  if (controller.get_digital_new_press(clamp_button) || controller.get_digital_new_press(alt_clamp_button)) {
     on = !on;
     activate_piston(clamp, on);
   }
 
-  if (clamp_sensor.get_new_press()) {
-    on = true;
-    activate_piston(clamp, on);
-    pros::lcd::set_text(0, "yay");
-    
-  }  
-  pros::lcd::set_text(3, std::to_string(pros::c::adi_digital_get_new_press(2)));
-
+  
 }
 
-pros::controller_digital_e_t lift_button = DIGITAL_B;
-void drive_lift(){
+pros::controller_digital_e_t lift_button = DIGITAL_RIGHT;
+pros::controller_digital_e_t alt_lift_button = DIGITAL_Y;
+
+void drive_lift() {
   static bool on = false;
-  if (controller.get_digital_new_press(lift_button)) {
+
+  if (controller.get_digital_new_press(lift_button) || controller.get_digital_new_press(alt_lift_button)) {
     on = !on;
-    activate_piston(right_lift, on);
+    activate_piston(lift, on);
   }
-}  
+
+ 
+}
 
 
-pros::controller_digital_e_t sp1 = DIGITAL_L1;
-pros::controller_digital_e_t sp2 = DIGITAL_L2;
+
+pros::controller_digital_e_t sp1 = DIGITAL_LEFT;
+pros::controller_digital_e_t sp2 = DIGITAL_A;
 void score_preload() {
   if (controller.get_digital(sp1) && controller.get_digital_new_press(sp2)) {
     motor_seconds(conveyor, 0.1, -127);
@@ -262,7 +272,10 @@ void score_preload() {
 
 
 // auton functions
-
+ 
+/**
+Moves chassis a specified amount of inches at a specified speed
+ */
 void move_inches(double inches, int speed) {
   pros::c::adi_encoder_reset(left_drive_encoder);
   pros::c::adi_encoder_reset(right_drive_encoder);
@@ -270,7 +283,7 @@ void move_inches(double inches, int speed) {
   wheels_speed(speed * sign(inches), speed * sign(inches));
 
   while (fabs(degrees_to_drive_inches(left_drive_encoder)) < fabs(inches)) {  
-    pros::delay(10);
+    pros::Task::delay(10);
   }
 
   wheels_speed(0, 0);
@@ -278,7 +291,7 @@ void move_inches(double inches, int speed) {
 
 
 /**
-Moves the robots left and right side a specified amount in inches at an accurate speed.
+Moves left and right side of chassis a specified amount of inches at an accurate speed.
 */
 void move_inches(double left_inches, double right_inches) {
   pros::c::adi_encoder_reset(left_drive_encoder);
@@ -298,13 +311,13 @@ void move_inches(double left_inches, double right_inches) {
       right_goal = true;
     }
     
-    pros::delay(10);
+    pros::Task::delay(10);
   }
   wheels_speed(0, 0);
 }
 
 /**
-Moves the drivetrain's left and right side a specified amount in inches at a specified speed.
+Moves left and right side of chassis a specified amount of inches at a specified speed.
 */
 void move_inches(double left_inches, double right_inches, int speed) {
   pros::c::adi_encoder_reset(left_drive_encoder);
@@ -324,11 +337,14 @@ void move_inches(double left_inches, double right_inches, int speed) {
       right_goal = true;
     }
 
-    pros::delay(10);
+    pros::Task::delay(10);
   }
   wheels_speed(0, 0);
 }
 
+/**
+Moves left and right side of chassis a specified amount of inches at different speeds.
+*/
 void move_inches(double left_inches, double left_speed, double right_inches, double right_speed) {
   pros::c::adi_encoder_reset(left_drive_encoder);
   pros::c::adi_encoder_reset(right_drive_encoder);
@@ -348,33 +364,33 @@ void move_inches(double left_inches, double left_speed, double right_inches, dou
     }
     controller.set_text(1, 1, std::to_string(fabs(degrees_to_drive_inches(left_drive_encoder))));
 
-    pros::delay(10);
+    pros::Task::delay(10);
   }
   wheels_speed(0, 0);
 }
 
 /**
-Moves the drivetrain at a specified speeds for a specified amount seconds.
+Moves the left and right of chassis at a specified speed for a specified amount seconds.
 */
 void move_seconds(float seconds, int left, int right) {
   wheels_speed(left, right);
 
-  pros::delay(seconds * 1000);
+  pros::Task::delay(seconds * 1000);
   wheels_speed(0, 0);
 }
 
 /**
-Turns the drivetrain towards the nearest object then stores the change in the imu value
+Turns the chassis towards the nearest object then stores the change in the imu value
 */
 int compensate() {
   static int margin = 15;
   int yaw = imu.get_heading();
   bool inMiddle = false;
   while (!inMiddle) {
-    if (pros::c::vision_get_by_sig(17, 0, 1).x_middle_coord < VISION_FOV_WIDTH / 2 - margin) {
+    if (vision_sensor.get_by_sig(0, 1).x_middle_coord < VISION_FOV_WIDTH / 2 - margin) {
       wheels_speed(-25, 25);
     }
-    else if (pros::c::vision_get_by_sig(17, 0, 1).x_middle_coord > VISION_FOV_WIDTH / 2 + margin) {
+    else if (vision_sensor.get_by_sig(0, 1).x_middle_coord > VISION_FOV_WIDTH / 2 + margin) {
       wheels_speed(25, -25);
     }
     else {
@@ -383,14 +399,14 @@ int compensate() {
       inMiddle = true;
     }
 
-    pros::delay(10);
+    pros::Task::delay(10);
   }
 
   return yaw;
 }
 
 /**
-Turns the drivetrain in a direction at a specified speed for a specified amount of seconds.
+Turns the chassis in a direction at a specified speed for a specified amount of seconds.
 */
 void turn_seconds(float seconds, int speed, bool right) {
   if (right)
@@ -398,7 +414,7 @@ void turn_seconds(float seconds, int speed, bool right) {
   else 
     wheels_speed(speed, -speed);
 
-  pros::delay(seconds * 1000);
+  pros::Task::delay(seconds * 1000);
   wheels_speed(0, 0);
 }
 
@@ -438,13 +454,14 @@ void hold_drivetrain(bool left_side, bool right_side, float seconds) {
         group_speed(drive_right_group, 0);
       }
     }
-    pros::delay(10);
+    pros::Task::delay(10);
     timer += 10;
   }
 }
 
 /**
-Turns the drivetrain until it reaches a specified degree using the imu. The imu is never tared.
+Turns the chassis until it reaches a specified degree using the imu. The imu is never tared so give it an absolute degree rather than a relative change.
+This will probably turn in the direction that will result in the least amount of turning
 */
 void turn_to(int degrees) {
   double startHeading = imu.get_heading();
@@ -472,6 +489,10 @@ void turn_to(int degrees) {
   controller.set_text(1, 1, std::to_string(imu.get_heading()));
 
 }
+
+/**
+Turns the chassis to an absoulte degree with a specified direction
+ */
 void turn_to(int degrees, bool right) {
   static int margin = 3;
   static int speed = 40;
@@ -482,11 +503,14 @@ void turn_to(int degrees, bool right) {
     wheels_speed(-speed, speed);
   }
   while (imu.get_heading() > degrees + margin || imu.get_heading() < degrees - margin) {
-    pros::delay(10);
+    pros::Task::delay(10);
   }
   wheels_speed(0, 0);
 }
 
+/**
+Turns the chassis to an absoulte degree with a specified direction at a specified speed
+ */
 void turn_to(int degrees, int speed, bool right) {
   static int margin = 3;
   if (right) {
@@ -496,34 +520,37 @@ void turn_to(int degrees, int speed, bool right) {
     wheels_speed(-speed, speed);
   }
   while (imu.get_heading() > degrees + margin || imu.get_heading() < degrees - margin) {
-    pros::delay(10);
-  }
-  wheels_speed(0, 0);
-}
-
-void turn_to(int degrees, int left_speed, int right_speed) {
-  static int margin = 3;
-  wheels_speed(left_speed, right_speed);
-  
-  while (imu.get_heading() > degrees + margin || imu.get_heading() < degrees - margin) {
-    pros::delay(10);
+    pros::Task::delay(10);
   }
   wheels_speed(0, 0);
 }
 
 /**
-Moves the drivetrain's left and right until it reaches a certain degree.
+Turns the chassis to an absoulte degree with a specified direction at individual left and right speed
+ */
+void turn_to(int degrees, int left_speed, int right_speed) {
+  static int margin = 3;
+  wheels_speed(left_speed, right_speed);
+  
+  while (imu.get_heading() > degrees + margin || imu.get_heading() < degrees - margin) {
+    pros::Task::delay(10);
+  }
+  wheels_speed(0, 0);
+}
+
+/**
+Moves the left and right of the chassis at a specified speed until it reaches a certain absolute degree is reached.
 */
 void move_until_degrees(int left_speed, int right_speed, int degrees) {
   if (degrees < imu.get_heading()) {
     wheels_speed(left_speed, right_speed);
     while (imu.get_heading() > degrees) 
-      pros::delay(10);
+      pros::Task::delay(10);
   }
   else {
     wheels_speed(left_speed, right_speed);
     while (imu.get_heading() < degrees) 
-      pros::delay(10);
+      pros::Task::delay(10);
   }
 
   wheels_speed(0, 0);
@@ -551,6 +578,9 @@ void move_intake(bool on, bool forward) {
   }
 }
 
+/**
+Turns the conveyor on or off in a specifed direction.
+*/
 void move_conveyor(bool on, bool forward) {
   if (on) {
     if (forward) {
@@ -564,17 +594,94 @@ void move_conveyor(bool on, bool forward) {
   }
 }
 
+/**
+This is a combination of move intake and move conveyor
+*/
 void move_intake_and_conveyor(bool on) {
   move_intake(on, true);
   move_conveyor(on, true);
 }
 
+/**
+Opens or closes the clamp
+*/
 void move_clamp(bool on) {
   activate_piston(clamp, on);
 }
 
+/**
+Moves any specified motor for a specifed amount of seconds
+ */
 void motor_seconds(pros::Motor motor, float seconds, int speed) {
   motor = speed;
-  pros::delay(seconds * 1000);
+  pros::Task::delay(seconds * 1000);
   motor = 0;
+}
+
+
+/**
+Turns the ejector task on or off
+*/
+bool ejectorEnabled = false;
+void enableEjector(bool on) {
+  ejectorEnabled = on;
+}
+
+/**
+The functionallity of the ejector
+*/
+void ejectorFun() {
+  static bool runOnce = true;
+
+  static int color;
+  if (runOnce) {
+    runOnce = false;
+    if (getColor()) {
+      color = 2;
+    }
+    else {
+      color = 1;
+    }
+  }
+  
+  pros::vision_object_s_t ring = vision_sensor.get_by_size(0);
+
+  pros::lcd::set_text(2, std::to_string(ring.height) + " " + std::to_string(ring.width));
+  if (ring.signature == color && ring.width > 150) {
+    activate_piston(ejector, true);
+  }
+  else {
+    activate_piston(ejector, false);
+  }
+}
+
+/**
+This is what is passed into the task
+ */
+void ejectorTask(){
+	while (ejectorEnabled) {
+    ejectorFun();
+    pros::Task::delay(20);
+  }
+}
+
+/**
+Sets the color that the ejector likes
+ */
+bool color = true;
+void setColor(bool red){
+  color = red;
+}
+
+/**
+Gets the color that the ejector likes
+ */
+bool getColor() {
+  return color;
+}
+
+void taskTest() {
+  while (true){
+    pros::lcd::set_background_color(100, 50, 70);
+  }
 }
