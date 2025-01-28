f3e7cbc4a41bb8b8e0588ec7bbb8752707cd9ee8
ratsareweird <alecninja5000@gmail.com>
2025-01-28 08:06:57 -0500

    Update initialize.cpp

--- initialize.cpp
+++ initialize.cpp
@@ -48,6 +48,7 @@ void autonomous() {
 
 	pros::Task task(ejectorTask, "Fun time");
 
+
 	Code code_number = SKILLS;
 
 	// alliance
@@ -300,117 +301,102 @@ void autonomous() {
 	}
 
 	else if (code_number == SKILLS) {
-		// score on wall stake
-		motor_seconds(conveyor, 1.2, 127);
-		motor_seconds(conveyor, 1.2, -127);
-
+		/*// score on wall stake
+		motor_seconds(conveyor, 0.4, 127);
+		motor_seconds(conveyor, 0.1, -127);
 
 		// get 1st ring
 		turn_to(30, 90, 30);
-
-		//move_inches(10, 60, 10, 90); change to new function
 		move_intake_and_conveyor(true);
 		move_inches(22, 80);
 		move_inches(10, 50);
-		turn_to(7, false);
 
+		// get stake
+		turn_to(7, false);
 		move_inches(-11, 80);
 		move_intake_and_conveyor(false);
 		move_inches(-8, 50);
 		move_clamp(true);
 	
+		// get ring 2
 		move_intake_and_conveyor(true);
-		turn_to(37, true);
+		turn_to(29, true);
 		move_inches(18, 80);
 		move_inches(7, 50);
 	
-		turn_to(40, -50, 60);
-		move_inches(11, 70);
-		move_inches(8, 30);
+		// get ring 3
+		turn_to(42, -50, 60);
+		move_inches(14, 70);
+		move_inches(10, 30);
+		pros::Task::delay(200);
 
-		move_inches(-12, 80);
-
+		// get ring 4, 5
+		move_inches(-9, 80);
 		turn_to(165, true);
-		move_inches(15, 80);
-		move_inches(12, 50);
-		move_inches(-18, 75);
+		move_inches(15, 75);
+		pros::Task::delay(300);
+		move_inches(10, 50);
+		move_seconds(0.2, 50, 50);
 
-		move_seconds(0.9, 30, 120);
-		pros::Task::delay(800);
-		//turn_to(65, 20, 50);
-		//turn_to(345, -80, 50);
-		move_seconds(0.7, -80, 50);
+		// get ring 6
+		move_inches(-16, 75);
+		turn_to(110, 0, 90);
+		move_inches(4, 70);
+		move_seconds(0.3, 50, 50);
 
-		move_seconds(0.7, -80, -80);
-
+		// score stake
+		turn_to(2, -60, 60);
+		move_inches(-18, 80);
+		pros::Task::delay(400);
 		move_clamp(false);
-		pros::Task::delay(1000);
-		move_seconds(0.5, 127, 127);
-
-		//move_inches(10, -80);
-
-		
-		
-		
-
-/*
-
-
-
-
-		// get stake
 		move_intake_and_conveyor(false);
 
-		turn_to(290, 80, false);
-		move_inches(15, -80);
+		// go to other side
+		move_inches(10, 80);
+		turn_to(280, false);
+		move_inches(50, 83, 80);
+
+		// get ring 7
+		turn_to(315);
+		move_intake_and_conveyor(true);
+		move_inches(25, 50);
+		move_intake_and_conveyor(false);
+
+		// get stake 2
+		turn_to(3, true);
+		move_inches(-12, 50);*/
 		move_clamp(true);
 
-		//get 2nd ring
-		turn_to(310, true);
+		// get ring 8
+		//turn_to(318, false);
+		imu.set_heading(315);
 		move_intake_and_conveyor(true);
-		move_inches(24, 90);
-		pros::Task::delay(300);
+		move_inches(25, 70);
 
+		// get ring 9
+		//turn_to(321, 40, -40);
+		move_inches(22, 60, 50);
 
-		// get 3rd ring
-		turn_to(307, false);
-		move_inches(15, 110);
-		move_inches(10, 60);
+		// get ring 10 and 11
+		turn_to(190, -70, 40);
+		move_inches(20, 70);
+		move_inches(10, 50);
+		move_seconds(0.3, 30, 30);
 
-		pros::Task::delay(300);
-
-		// get 4th/5th ring
-		move_inches(-4, 100);
-		turn_to(68, 35, -70);
-		move_inches(20, 100);
-		move_inches(20, 50);
-
-		// get 6th ring
-		move_inches(-26, 100);
-		//move_inches(10, 30, 30, 80); change to new function
-		move_inches(10, 30, 80);
-		move_inches(-2, 100);
-
-
-		// drop stake
-		turn_to(240, false);
-		move_seconds(0.5, -127, -127);
-		move_clamp(false);
-		move_seconds(0.5, -127, -127);
-		move_clamp(false);
-		move_seconds(0.3, 127, 127);
+		// get ring 12
+		move_inches(-16, 70);
+		turn_to(265, 90, 0);
+		move_inches(5, 50);
 
 
 		move_intake_and_conveyor(false);
-		*/
-
 	}
 
 	else if (code_number == TEST) {
 		pros::Task::delay(1000000);
 	}
 
-	task.suspend();
+	task.remove();
 
 }
 
@@ -429,11 +415,7 @@ void opcontrol() {
 		drive_intake();
 		drive_lift();
 
-		get_seen_color();
-
-		//score_preload();
-
-		//ejectorFun();
+		controller.set_text(1, 1, std::to_string(conveyor.get_position()));
 
 		pros::Task::delay(20);
 	}
